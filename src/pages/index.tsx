37cda557e4a77118fc892615ee878b552fc433a3
phuongkieu <phuongkhn@zigvy.com>
2023-03-28 23:24:01 +0700

    config alias import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,17 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../../public/dev-ed-wave.png";
-import code from "../../public/code.png";
-import design from "../../public/design.png";
-import consulting from "../../public/consulting.png";
 import Image from "next/image";
-import web1 from "../../public/web1.png";
-import web2 from "../../public/web2.png";
-import web3 from "../../public/web3.png";
-import web4 from "../../public/web4.png";
-import web5 from "../../public/web5.png";
-import web6 from "../../public/web6.png";
+import deved from "@/images/dev-ed-wave.png";
+import code from "@/images/code.png";
+import design from "@/images/design.png";
+import consulting from "@/images/consulting.png";
+import web1 from "@/images/web1.png";
+import web2 from "@/images/web2.png";
+import web3 from "@/images/web3.png";
+import web4 from "@/images/web4.png";
+import web5 from "@/images/web5.png";
+import web6 from "@/images/web6.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
