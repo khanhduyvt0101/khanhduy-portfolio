6e3587c8bb86474b31359a2f99cc9cef460b62c9
Duy Bui <khanhduyvt0101@Mac-mini.local>
2023-06-29 22:03:29 +0700

    update preview image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,41 @@
 import Head from "next/head";
-import { HomeScreen } from "@/pages/Home";
+import { HomeScreen } from "@/Home";
+import avatarImage from "@/images/avatar.jpeg";
 
 export default function Home() {
   return (
-    <div className={""}>
+    <>
       <Head>
+        <meta name="description" content="khanhduyvt0101@gmail.com" />
         <title>Khanh Duy Portfolio</title>
         <meta name="description" content="khanhduyvt0101@gmail.com" />
+        {/* Facebook */}
+        <meta
+          property="og:url"
+          content="https://khanhduyvt-portfolio.netlify.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Khanh Duy Portfolio" />
+        <meta property="og:description" content="khanhduyvt0101@gmail.com" />
+        <meta property="og:image" content={avatarImage.src}></meta>
         <link rel="icon" href="/avatarHeader.svg" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="khanhduyvt-portfolio.netlify.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://khanhduyvt-portfolio.netlify.app/"
+        />
+        <meta name="twitter:title" content="Khanh Duy Portfolio" />
+        <meta name="twitter:description" content="khanhduyvt0101@gmail.com" />
+        <meta name="twitter:image" content={avatarImage.src}></meta>
       </Head>
       <main className=" bg-black h-full w-full">
         <HomeScreen />
       </main>
-    </div>
+    </>
   );
 }
