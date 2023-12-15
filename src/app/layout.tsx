aed6f4ed5e0760575cc0c25d193b91331be9f26d
Duy Bui <khanhduyvt0101@gmail.com>
2023-12-15 22:52:15 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,27 +5,6 @@ import Header from "@/components/header";
 import BackToTop from "@/components/back-to-top";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
-export const metadata: Metadata = {
-  title: "KhanhDuy | Personal",
-  authors: [{ name: "Khanh Duy", url: "https://github.com/khanhduyvt0101" }],
-  description: "Khanh Duy's personal portfolio website",
-  openGraph: {
-    title: "Khanh Duy | Personal",
-    description: "Khanh Duy's personal portfolio website",
-    url: process.env.METADATA_BASE,
-    images: [
-      {
-        url: `${process.env.METADATA_BASE}/avatar.png`,
-        alt: "Khanh Duy | Personal",
-        width: 600,
-        height: 600,
-      },
-    ],
-  },
-  alternates: {
-    canonical: process.env.METADATA_BASE,
-  },
-};
 
 export default function RootLayout({
   children,
@@ -34,9 +13,40 @@ export default function RootLayout({
   children: React.ReactNode;
   pageMetadata: Metadata;
 }) {
-  const meta = { ...metadata, ...pageMetadata };
   return (
     <html lang="en">
+      <head>
+        <title>Khanh Duy | Personal</title>
+        <meta
+          name="description"
+          content="Khanh Duy's personal portfolio website"
+        />
+
+        <meta property="og:url" content="https://www.khanhduy.site/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Khanh Duy | Personal" />
+        <meta
+          property="og:description"
+          content="Khanh Duy's personal portfolio website"
+        />
+        <meta
+          property="og:image"
+          content="https://www.khanhduy.site/avatar.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="khanhduy.site" />
+        <meta property="twitter:url" content="https://www.khanhduy.site/" />
+        <meta name="twitter:title" content="Khanh Duy | Personal" />
+        <meta
+          name="twitter:description"
+          content="Khanh Duy's personal portfolio website"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.khanhduy.site/avatar.png"
+        />
+      </head>
       <body className={montserrat.className}>
         <Header />
         <main className="container lg:px-28">{children}</main>
