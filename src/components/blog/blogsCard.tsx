611fca0707aa6e287e13c8c9fafe6b77c96e9a4e
Duy Bui <khanhduyvt0101@gmail.com>
2023-12-17 09:46:38 +0700

    update css for blog card

--- src/components/blog/blogsCard.tsx
+++ src/components/blog/blogsCard.tsx
@@ -22,16 +22,24 @@ const BlogCard: React.FC<BlogCardProps> = ({ blog, isDark }) => {
   };
 
   return (
-    <div
-      onClick={() => openUrlInNewTab(blog.link, blog.title)}
-      className={`${
-        isDark ? "bg-gray-800" : "bg-white"
-      } p-4 rounded-sm shadow-md w-[350px] h-[220px] relative hover:shadow-2xl flex flex-col items-center`}
-    >
-      <h3 className="text-lg font-semibold">{blog.title}</h3>
-      <p className="text-sm line-clamp-5">{blog.description}</p>
-      <div className="absolute top-0 right-0 flex items-center justify-center w-8 h-8 overflow-hidden bg-moreProjectsButton rounded-[0_16px_0_128px]">
-        <div className="-mt-1 -mr-1 text-white font-[courier,sans]">→</div>
+    <div onClick={() => openUrlInNewTab(blog.link, blog.title)}>
+      <div className={isDark ? "blog-container dark-mode" : "blog-container"}>
+        <a
+          className={
+            isDark ? "dark-mode blog-card blog-card-shadow" : "blog-card"
+          }
+          href="#blog"
+        >
+          <h3 className={isDark ? "small-dark blog-title" : "blog-title"}>
+            {blog.title}
+          </h3>
+          <p className={isDark ? "small-dark small" : "small"}>
+            {blog.description}
+          </p>
+          <div className="go-corner">
+            <div className="go-arrow">→</div>
+          </div>
+        </a>
       </div>
     </div>
   );
