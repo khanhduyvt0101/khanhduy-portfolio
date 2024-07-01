70edbcf3cfe3f6208abf43a60e365e06a7c0fecc
Duy Bui <khanhduyvt0101@gmail.com>
2024-07-01 17:30:45 +0700

    update terms

---
+++ app/sieutantinh/terms/page.tsx
@@ -0,0 +1,199 @@
+import { ReactNode } from "react";
+
+export default function Page(): ReactNode {
+  return (
+    <div className="px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          THỎA THUẬN GIẤY PHÉP NGƯỜI DÙNG CUỐI (EULA) CHO SIEU TAN TINH
+        </h1>
+
+        <p>
+          {`Thỏa Thuận Giấy Phép Người Dùng Cuối ("Thỏa Thuận") này là một thỏa thuận pháp lý
+          giữa bạn ("Người Dùng" hoặc "Bạn") và Duy Bui ("Người Cấp Phép," "Tôi," hoặc "Chúng Tôi")
+          để sử dụng ứng dụng Sieu Tan Tinh ("Ứng Dụng Được Cấp Phép").
+          Bằng cách tải xuống, cài đặt hoặc sử dụng Ứng Dụng Được Cấp Phép, bạn
+          đồng ý bị ràng buộc bởi các điều khoản của Thỏa Thuận này. Nếu bạn không đồng ý
+          với các điều khoản của Thỏa Thuận này, đừng tải xuống, cài đặt hoặc sử dụng
+          Ứng Dụng Được Cấp Phép.`}
+        </p>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          1. Cấp Phép
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Người Cấp Phép cấp cho bạn một giấy phép không độc quyền, không thể
+            chuyển nhượng, có thể thu hồi để sử dụng Ứng Dụng Được Cấp Phép trên
+            bất kỳ sản phẩm nào của Apple mà bạn sở hữu hoặc kiểm soát, như được
+            phép bởi Quy Định Sử Dụng được nêu trong Điều Khoản và Điều Kiện của
+            App Store. Giấy phép này không cho phép bạn sử dụng Ứng Dụng Được
+            Cấp Phép trên bất kỳ thiết bị Apple nào mà bạn không sở hữu hoặc
+            kiểm soát, và bạn không được phân phối hoặc làm cho Ứng Dụng Được
+            Cấp Phép có sẵn qua mạng nơi nó có thể được sử dụng bởi nhiều thiết
+            bị cùng một lúc.
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          2. Giới Hạn Tuổi
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Ứng Dụng Được Cấp Phép được dự định sử dụng bởi những người từ 18
+            tuổi trở lên. Bằng cách sử dụng Ứng Dụng Được Cấp Phép, bạn khẳng
+            định và đảm bảo rằng bạn ít nhất 18 tuổi.
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          3. Phạm Vi Giấy Phép
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Bạn không được thuê, cho thuê, cho mượn, bán, phân phối lại, hoặc
+            cấp phép lại Ứng Dụng Được Cấp Phép. Bạn không được sao chép (trừ
+            khi được phép rõ ràng bởi giấy phép này và Quy Định Sử Dụng), dịch
+            ngược, đảo ngược kỹ thuật, tháo rời, cố gắng lấy mã nguồn của, chỉnh
+            sửa, hoặc tạo các tác phẩm phái sinh của Ứng Dụng Được Cấp Phép, bất
+            kỳ bản cập nhật nào, hoặc bất kỳ phần nào của nó (trừ khi và chỉ
+            trong phạm vi bất kỳ hạn chế nào bị cấm bởi luật hiện hành hoặc
+            trong phạm vi được phép bởi các điều khoản cấp phép điều chỉnh việc
+            sử dụng bất kỳ thành phần mã nguồn mở nào được bao gồm trong Ứng
+            Dụng Được Cấp Phép).
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          4. Sự Đồng Ý Sử Dụng Dữ Liệu
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Bạn đồng ý rằng Người Cấp Phép có thể thu thập và sử dụng dữ liệu kỹ
+            thuật và thông tin liên quan — bao gồm nhưng không giới hạn thông
+            tin kỹ thuật về thiết bị của bạn, hệ thống và phần mềm ứng dụng, và
+            các thiết bị ngoại vi — được thu thập định kỳ để hỗ trợ cung cấp các
+            cập nhật phần mềm, hỗ trợ sản phẩm, và các dịch vụ khác cho bạn (nếu
+            có) liên quan đến Ứng Dụng Được Cấp Phép. Người Cấp Phép có thể sử
+            dụng thông tin này, miễn là nó ở dạng không xác định cá nhân bạn, để
+            cải thiện sản phẩm của mình hoặc cung cấp các dịch vụ hoặc công nghệ
+            cho bạn.
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          5. Chấm Dứt
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Thỏa Thuận này có hiệu lực đến khi bị chấm dứt bởi bạn hoặc Người
+            Cấp Phép. Quyền của bạn theo Thỏa Thuận này sẽ tự động chấm dứt mà
+            không cần thông báo từ Người Cấp Phép nếu bạn không tuân thủ bất kỳ
+            điều khoản nào của Thỏa Thuận này. Khi Thỏa Thuận chấm dứt, bạn phải
+            ngừng tất cả việc sử dụng Ứng Dụng Được Cấp Phép và tiêu hủy tất cả
+            các bản sao, toàn bộ hoặc một phần, của Ứng Dụng Được Cấp Phép.
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          6. Dịch Vụ Bên Ngoài
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            {` Ứng Dụng Được Cấp Phép có thể cho phép truy cập vào các dịch vụ và trang web của Người Cấp Phép và/hoặc
+            bên thứ ba (gọi chung và riêng là "Dịch Vụ Bên Ngoài"). Bạn đồng ý sử dụng Dịch Vụ Bên Ngoài
+            với rủi ro của riêng bạn. Người Cấp Phép không chịu trách nhiệm kiểm tra hoặc đánh giá
+            nội dung hoặc độ chính xác của bất kỳ Dịch Vụ Bên Ngoài nào của bên thứ ba,
+            và sẽ không chịu trách nhiệm cho bất kỳ Dịch Vụ Bên Ngoài nào của bên thứ ba đó.
+            Dữ liệu được hiển thị bởi bất kỳ Ứng Dụng Được Cấp Phép hoặc Dịch Vụ Bên Ngoài nào,
+            bao gồm nhưng không giới hạn dữ liệu tài chính, y tế, và thông tin vị trí,
+            chỉ mang tính chất thông tin chung và không được đảm bảo bởi Người Cấp Phép hoặc các đại lý của mình.`}
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          7. KHÔNG CÓ BẢO HÀNH
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            {`BẠN RÕ RÀNG THỪA NHẬN VÀ ĐỒNG Ý RẰNG VIỆC SỬ DỤNG ỨNG DỤNG ĐƯỢC CẤP PHÉP
+            LÀ RỦI RO CỦA RIÊNG BẠN. TRONG MỨC ĐỘ TỐI ĐA ĐƯỢC PHÁP LUẬT ÁP DỤNG CHO PHÉP,
+            ỨNG DỤNG ĐƯỢC CẤP PHÉP VÀ BẤT KỲ DỊCH VỤ NÀO ĐƯỢC THỰC HIỆN
+            HOẶC CUNG CẤP BỞI ỨNG DỤNG ĐƯỢC CẤP PHÉP ĐƯỢC CUNG CẤP "NGUYÊN TRẠNG" VÀ "NHƯ CÓ SẴN",
+            VỚI TẤT CẢ CÁC LỖI VÀ KHÔNG CÓ BẢO HÀNH NÀO, VÀ NGƯỜI CẤP PHÉP TỪ CHỐI TẤT CẢ CÁC BẢO HÀNH
+            VÀ ĐIỀU KIỆN LIÊN QUAN ĐẾN ỨNG DỤNG ĐƯỢC CẤP PHÉP VÀ CÁC DỊCH VỤ NÀO, DÙ LÀ RÕ RÀNG,
+            NGỤ Ý, HOẶC THEO LUẬT ĐỊNH, BAO GỒM, NHƯNG KHÔNG GIỚI HẠN ĐẾN, CÁC BẢO HÀNH NGỤ Ý VÀ/HOẶC
+            CÁC ĐIỀU KIỆN VỀ KHẢ NĂNG BÁN ĐƯỢC, CHẤT LƯỢNG ĐẠT YÊU CẦU, SỰ PHÙ HỢP CHO MỘT MỤC ĐÍCH CỤ THỂ,
+            ĐỘ CHÍNH XÁC, SỰ THOẢI MÁI, VÀ KHÔNG VI PHẠM CÁC QUYỀN CỦA BÊN THỨ BA. KHÔNG CÓ THÔNG TIN HOẶC
+            LỜI KHUYÊN NÀO BẰNG MIỆNG HOẶC BẰNG VĂN BẢN ĐƯỢC CUNG CẤP BỞI NGƯỜI CẤP PHÉP HOẶC ĐẠI DIỆN
+            ĐƯỢC ỦY QUYỀN CỦA NÓ SẼ TẠO RA MỘT BẢO HÀNH. NẾU ỨNG DỤNG ĐƯỢC CẤP PHÉP HOẶC CÁC DỊCH VỤ
+            CHỨNG MINH BỊ LỖI, BẠN CHỊU TOÀN BỘ CHI PHÍ CỦA VIỆC SỬA CHỮA, SỬA CHỮA HOẶC SỬA CHỮA.`}
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          8. Giới Hạn Trách Nhiệm
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            TRONG PHẠM VI KHÔNG BỊ CẤM BỞI LUẬT PHÁP, TRONG MỌI TRƯỜNG HỢP,
+            NGƯỜI CẤP PHÉP KHÔNG CHỊU TRÁCH NHIỆM VỀ THƯƠNG TÍCH CÁ NHÂN HOẶC
+            BẤT KỲ THIỆT HẠI NÀO, ĐẶC BIỆT, GIÁN TIẾP, HOẶC HẬU QUẢ NÀO, BAO
+            GỒM, NHƯNG KHÔNG GIỚI HẠN ĐẾN, THIỆT HẠI VỀ MẤT LỢI NHUẬN, MẤT DỮ
+            LIỆU, GIÁN ĐOẠN KINH DOANH, HOẶC BẤT KỲ THIỆT HẠI HOẶC MẤT THƯƠNG
+            MẠI NÀO KHÁC, PHÁT SINH TỪ HOẶC LIÊN QUAN ĐẾN VIỆC SỬ DỤNG HOẶC
+            KHÔNG THỂ SỬ DỤNG ỨNG DỤNG ĐƯỢC CẤP PHÉP, DÙ BẤT KỲ LÝ THUYẾT TRÁCH
+            NHIỆM NÀO (HỢP ĐỒNG, SAI LẦM, HOẶC KHÁC) VÀ NGAY CẢ KHI NGƯỜI CẤP
+            PHÉP ĐÃ ĐƯỢC THÔNG BÁO VỀ KHẢ NĂNG CỦA CÁC THIỆT HẠI ĐÓ. TRONG MỌI
+            TRƯỜNG HỢP, TỔNG TRÁCH NHIỆM CỦA NGƯỜI CẤP PHÉP ĐỐI VỚI BẠN CHO TẤT
+            CẢ CÁC THIỆT HẠI KHÔNG VƯỢT QUÁ SỐ TIỀN NĂM MƯƠI ĐÔ LA ($50.00).
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          9. Kiểm Soát Xuất Khẩu
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            {` Bạn không được sử dụng hoặc xuất khẩu hoặc tái xuất khẩu Ứng Dụng Được Cấp Phép
+            ngoại trừ khi được phép bởi luật pháp Hoa Kỳ và luật pháp của khu vực tài phán nơi Ứng Dụng Được Cấp Phép được mua.
+            Đặc biệt, nhưng không giới hạn, Ứng Dụng Được Cấp Phép không được xuất khẩu hoặc tái xuất khẩu (a) vào bất kỳ nước nào bị cấm vận bởi Hoa Kỳ
+            hoặc (b) cho bất kỳ ai nằm trong Danh Sách Đặc Biệt Chỉ Định của Bộ Tài Chính Hoa Kỳ hoặc Danh Sách Những Người Bị Từ Chối
+            hoặc Danh Sách Các Thực Thể của Bộ Thương Mại Hoa Kỳ. Bằng việc sử dụng Ứng Dụng Được Cấp Phép, bạn
+            tuyên bố và đảm bảo rằng bạn không nằm trong bất kỳ nước nào như vậy hoặc trong bất kỳ danh sách nào như vậy.`}
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          10. Luật Điều Chỉnh
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Thỏa Thuận này và mối quan hệ giữa bạn và Người Cấp Phép sẽ được
+            điều chỉnh bởi luật pháp của Bang California, loại trừ các quy định
+            xung đột pháp luật của nó. Bạn và Người Cấp Phép đồng ý đệ trình lên
+            quyền tài phán cá nhân và độc quyền của các tòa án nằm trong quận
+            Santa Clara, California, để giải quyết bất kỳ tranh chấp hoặc yêu
+            cầu nào phát sinh từ Thỏa Thuận này.
+          </p>
+        </div>
+
+        <h3 className="mt-10 text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+          11. Thông Tin Liên Hệ
+        </h3>
+        <div className="mt-2 max-w-2xl">
+          <p>
+            Nếu bạn có bất kỳ câu hỏi nào về Thỏa Thuận này, vui lòng liên hệ
+            với tôi tại khanhduyvt0101@gmail.com.
+          </p>
+        </div>
+
+        <p>
+          Bằng cách sử dụng ứng dụng Sieu Tan Tinh, bạn thừa nhận rằng bạn đã
+          đọc Thỏa Thuận này, hiểu nó và đồng ý bị ràng buộc bởi các điều khoản
+          và điều kiện của nó.
+        </p>
+      </div>
+    </div>
+  );
+}
