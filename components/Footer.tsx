8364580f11e4f1d44d0ef24628d944ce1f06b1c3
Duy Bui <khanhduyvt0101@gmail.com>
2024-09-01 10:45:54 +0700

    update header/footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,7 +29,7 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
+                <NavLink href="https://khanhduy.com/">Blog</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Khanh Duy. All rights
