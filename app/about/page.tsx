ea60469038125d767ad4708b85c9183124e50bea
Bui Trong Khanh Duy <khanhduyvt0101@gmail.com>
2024-11-03 11:44:14 +0700

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -29,7 +29,7 @@ export default function About() {
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             My name is Khanh Duy, I&apos;m 24 years old, and I reside in Ho Chi
-            Minh City
+            Minh City.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
