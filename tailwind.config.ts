79bc6f8393f9e96b400a4ac5b48245cda8c0a06e
Ethan Bui <ethanbui@Ethans-Mac-mini.local>
2026-03-04 00:32:22 +0700

    fix: resolve tailwind css content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: [],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
