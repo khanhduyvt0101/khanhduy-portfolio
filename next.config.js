fd54ec9df8aca058aaf407324a5c23f548260e54
Ethan Bui <ethanbui@Ethans-Mac-mini.local>
2026-03-04 00:35:59 +0700

    chore: remove mantine from next config optimizePackageImports

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
 import createBundleAnalyzer from "@next/bundle-analyzer";
 
 function on(value) {
-  if (!value) return false;
-  const lowerCaseValue = value.toLowerCase();
-  return (
-    lowerCaseValue === "1" ||
-    lowerCaseValue === "true" ||
-    lowerCaseValue === "on" ||
-    lowerCaseValue === "yes" ||
-    lowerCaseValue === "y"
-  );
+	if (!value) return false;
+	const lowerCaseValue = value.toLowerCase();
+	return (
+		lowerCaseValue === "1" ||
+		lowerCaseValue === "true" ||
+		lowerCaseValue === "on" ||
+		lowerCaseValue === "yes" ||
+		lowerCaseValue === "y"
+	);
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  logging: on(process.env.LOG_FETCH)
-    ? { fetches: { fullUrl: true } }
-    : undefined,
-  experimental: {
-    optimizePackageImports: [
-      "@mantine/core",
-      "@mantine/hooks",
-      "@mantine/charts",
-    ],
-  },
+	typescript: { ignoreBuildErrors: true },
+	eslint: { ignoreDuringBuilds: true },
+	logging: on(process.env.LOG_FETCH)
+		? { fetches: { fullUrl: true } }
+		: undefined,
+	experimental: {
+		optimizePackageImports: ["@tabler/icons-react", "lucide-react"],
+	},
 };
 
 const withBundleAnalyzer = createBundleAnalyzer({
-  enabled: on(process.env.ANALYZE_BUNDLE),
+	enabled: on(process.env.ANALYZE_BUNDLE),
 });
 
 export default withBundleAnalyzer(nextConfig);
